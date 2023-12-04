f0058b17e1477a604147138709af47bec2868d2c
kritim36 <kritimallik4@gmail.com>
2023-12-04 19:47:58 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+
+import './App.css'
+import{BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from './pages/Home/Home'
+import CreateBlog from './pages/CreateBlog/CreateBlog'
+import SingleBlog from './pages/SingleBlog/SingleBlog'
+import UpdateBlog from './pages/UpdateBlog/UpdateBlog'
+
+function App() {
+  
+  return (
+    <>
+     <BrowserRouter>
+     <Routes>
+      <Route path ='/' element = {<Home />} />
+      <Route path ='/createBlog' element = {<CreateBlog />} />
+      <Route path ='/singleBlog' element = {<SingleBlog />} />
+      <Route path ='/updateBlog' element = {<UpdateBlog />} />
+     </Routes>
+     </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Home.css'
+import Navbar from '../../components/Navbar/Navbar'
+
+const Home = () => {
+  return (
+    <>
+   <Navbar />
+   <div className="card" style={{width: '18rem'}}>
+  
+  <div className="card-body">
+    <h5 className="card-title">Card title</h5>
+    <h5 className="card-title">Card subTitle</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+   </>
+  )
+}
+
+export default Home
